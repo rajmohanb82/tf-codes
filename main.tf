e4e7001509c3a917e6f6229c76418a9c43a613eb
rajmohanb82 <rajmohanb.gcp@gmail.com>
2024-01-27 00:47:57 +0530

    third commit

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # change this to your preferred AWS region
+  access_key = var.aws_acces_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "ubuntu_instance" {
