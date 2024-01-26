6ac7764f2dc33b3b8b314f8c1a37199b6046a52b
Raj Mohan Bharathi <91312695+rajmohanb82@users.noreply.github.com>
2024-01-27 01:26:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,9 @@ provider "aws" {
 resource "aws_instance" "ubuntu_instance" {
   ami           = "ami-0c7217cdde317cfec"  # replace with the latest Ubuntu 22.04 AMI in your region
   instance_type = "t3.medium"
-
-  # Use an existing security group
-  vpc_security_group_ids = ["sg-0dff307bf4b685399"]  # replace with your existing security group ID
-
+  vpc_id = "vpc-0a1b06f6fd08d6dae"
+  vpc_security_group_ids = ["sg-0dff307bf4b685399"]
   associate_public_ip_address = true  # enable public IP
-
   tags = {
     Name = "webserver"
   }
